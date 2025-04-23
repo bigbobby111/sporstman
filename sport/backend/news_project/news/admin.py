91d601a0607e358866944af3d90387b631374734
bigbobby111 <arsenijsena@gmail.com>
2025-04-23 13:22:15 +0500

    Delete sport directory

--- sport/backend/news_project/news/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import NewsItem
-
-@admin.register(NewsItem)
-class NewsItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'time', 'is_slider')
-    list_filter = ('category', 'is_slider')
-    search_fields = ('title', 'description')
--- sport/backend/news_project/news/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import NewsItem
-
-
-class NewsItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NewsItem
-        fields = '__all__'
